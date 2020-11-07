935ebec4edcd4cc072abbcf3d5e9beeb460752d2
Anchou Bockhorn <anchou.bockhorn@gmail.com>
2020-11-07 20:51:15 +0100

    return string under /

---
+++ src/main/java/ch/bemyquarantine/bmqapi/http/HttpUser.java
@@ -0,0 +1,12 @@
+package ch.bemyquarantine.bmqapi.http;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HttpUser {
+    @GetMapping("/")
+    String all() {
+        return "banana";
+    }
+}
