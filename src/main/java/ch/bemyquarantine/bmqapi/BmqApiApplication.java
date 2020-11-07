1400dd797583ce0fe159dce4775b65106c145658
Anchou Bockhorn <anchou.bockhorn@gmail.com>
2020-11-07 20:47:34 +0100

    init bmp api project

---
+++ src/main/java/ch/bemyquarantine/bmqapi/BmqApiApplication.java
@@ -0,0 +1,13 @@
+package ch.bemyquarantine.bmqapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmqApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BmqApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/bemyquarantine/bmqapi/BmqApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bemyquarantine.bmqapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmqApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
