7c013d9e3e44ae2ccf70d47311c1b6e0cd156aa0
Anchou Bockhorn <anchou.bockhorn@gmail.com>
2020-11-17 21:25:51 +0100

    configure github oAuth and add static js for testing

--- src/main/java/ch/bemyquarantine/bmqapi/BmqApiApplication.java
+++ src/main/java/ch/bemyquarantine/bmqapi/BmqApiApplication.java
@@ -2,12 +2,28 @@ package ch.bemyquarantine.bmqapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @SpringBootApplication
-public class BmqApiApplication {
+public class BmqApiApplication extends WebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(BmqApiApplication.class, args);
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(a -> a
+                        .antMatchers("/login.html", "/error").permitAll() // todo: implement /error
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .oauth2Login();
+    }
 }
--- src/main/java/ch/bemyquarantine/bmqapi/http/HttpUser.java
+++ src/main/java/ch/bemyquarantine/bmqapi/http/HttpUser.java
@@ -1,12 +1,22 @@
 package ch.bemyquarantine.bmqapi.http;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+import java.util.Map;
+
 @RestController
 public class HttpUser {
-    @GetMapping("/")
-    String all() {
-        return "bananas chiquita";
+    @GetMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("name", principal.getAttribute("login"));
+    }
+
+    @GetMapping("/more")
+    public Map<String, Object> more(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("name", principal.getAttribute("url"));
     }
 }
