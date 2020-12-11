0bef3aaf9e767e9795853dfc9568ce57f66d8fac
Anchou Bockhorn <anchou.bockhorn@gmail.com>
2020-12-11 19:30:29 +0100

    do not create users on startup

--- src/main/java/ch/bemyquarantine/bmqapi/persistance/Setup.java
+++ src/main/java/ch/bemyquarantine/bmqapi/persistance/Setup.java
@@ -20,12 +20,13 @@ public class Setup {
     InitializingBean seedDb(UserRepo userRepo) {
         return () -> {
             userRepo.deleteAll();
+/*
             userRepo.save(new User()
                     .setActive(true)
                     .setDateOfBirth(new GregorianCalendar(1994, Calendar.AUGUST, 17).getTime())
                     .setGender(Gender.MALE)
                     .setRelationTyp(RelationTyp.POLYGAMOUS)
-                    .setResidence(new Point(NamedCoordinateReferenceSystem.CRS_84, new Position(-73.564453125, 42.114523952464246)))
+//                    .setResidence(new Point(NamedCoordinateReferenceSystem.CRS_84, new Position(-73.564453125, 42.114523952464246)))
                     .setUsername("Rabia"));
 
             User user2 = new User()
@@ -33,9 +34,10 @@ public class Setup {
                     .setDateOfBirth(new GregorianCalendar(1991, Calendar.NOVEMBER, 28).getTime())
                     .setGender(Gender.MALE)
                     .setRelationTyp(RelationTyp.POLYGAMOUS)
-                    .setResidence(new Point(NamedCoordinateReferenceSystem.CRS_84, new Position(-73.264453125, 42.214523952464246)))
+//                    .setResidence(new Point(NamedCoordinateReferenceSystem.CRS_84, new Position(-73.264453125, 42.214523952464246)))
                     .setUsername("Julio");
             userRepo.save(user2);
+*/
         };
     }
 
